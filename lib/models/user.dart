8b1cc36dab12a751ddb11aa87c28290e9a5055b2
AndreasH <Andreashavnen@gmail.com>
2023-10-27 14:48:36 +0200

    Added basic user model

---
+++ lib/models/user.dart
@@ -0,0 +1,30 @@
+class User {
+  final String id;
+  final String username;
+  final String email;
+  final String password;
+  final int age;
+  final UserAddress address;
+
+  User(
+      {required this.id,
+      required this.username,
+      required this.email,
+      required this.password,
+      required this.age,
+      required this.address});
+}
+
+class UserAddress {
+  final String street;
+  final String city;
+  final String zipCode;
+  final String country;
+
+  UserAddress({
+    required this.street,
+    required this.city,
+    required this.zipCode,
+    required this.country,
+  });
+}
