72a6f79633c8e2cde5b6a3978c2de7f9f1b9dac4
AndreasH <Andreashavnen@gmail.com>
2023-10-27 17:37:32 +0200

    added border to app bar profile icon

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -11,23 +11,35 @@ class CategoriesScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Categories'),
         actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {
-              // Handle the button's action (e.g., add category)
-            },
+          Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                width: 2, // Border width
+              ),
+            ),
+            child: IconButton(
+              icon: Icon(
+                Icons.person,
+                size: 32,
+              ),
+              onPressed: () {
+                // Handle the button's action (e.g., open user profile)
+              },
+            ),
           ),
         ],
       ),
       body: GridView(
         gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 4,
+          childAspectRatio:
+              3 / 4, // Adjust the aspect ratio for better item spacing
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
         children: [
-          for (final animal in dummyAnimals) CategoryGridItem(animal: animal),
+          for (final animal in dummyAnimals) CategoryGridItem(animal: animal)
         ],
       ),
     );
