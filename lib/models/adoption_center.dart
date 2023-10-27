220d3e399701cc2b67be137e0c5532322e9ec005
AndreasH <Andreashavnen@gmail.com>
2023-10-27 14:52:54 +0200

    Added basic adoption center model.  Removed animal LatLng location

---
+++ lib/models/adoption_center.dart
@@ -0,0 +1,33 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class AdoptionCenter {
+  final String id;
+  final String name;
+  final String description;
+  final AdoptionCenterLocation location;
+  final List<String> availableAnimalIds;
+
+  AdoptionCenter({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.location,
+    required this.availableAnimalIds,
+  });
+}
+
+class AdoptionCenterLocation {
+  final LatLng location;
+  final String street;
+  final String city;
+  final String zipCode;
+  final String country;
+
+  AdoptionCenterLocation({
+    required this.location,
+    required this.street,
+    required this.city,
+    required this.zipCode,
+    required this.country,
+  });
+}
--- lib/models/animal.dart
+++ lib/models/animal.dart
@@ -1,5 +1,3 @@
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-
 class Animal {
   final String name;
   final String imageUrl;
@@ -9,7 +7,6 @@ class Animal {
   final String color;
   final int age;
   final String activity_level;
-  final LatLng location;
 
   Animal({
     required this.name,
@@ -20,6 +17,5 @@ class Animal {
     required this.color,
     required this.age,
     required this.activity_level,
-    required this.location,
   });
 }
