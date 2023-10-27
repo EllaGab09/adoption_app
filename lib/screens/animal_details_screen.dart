e71562b4b8e944bf4ea2e752538f99f1ba68fe89
AndreasH <Andreashavnen@gmail.com>
2023-10-27 21:45:06 +0200

    Update animal_details_screen.dart

--- lib/screens/animal_details_screen.dart
+++ lib/screens/animal_details_screen.dart
@@ -10,46 +10,65 @@ class AnimalDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(animal.name), // Set the title of the animal
+        title: Text(animal.name),
       ),
-      body: Column(
-        children: [
-          // Display the animal's image
-          Image.network(
-            animal.imageUrl,
-            fit: BoxFit.cover,
-            height: 200, // Adjust the height as needed
-            width: double.infinity,
+      body: ListView(
+        children: <Widget>[
+          Container(
+            height: 250, // Set the image container height
+            child: Image.network(
+              animal.imageUrl,
+              fit: BoxFit.cover,
+              width: double.infinity,
+            ),
           ),
-          ListTile(
-            leading: Icon(Icons.description),
-            title: Text("Description"),
-            subtitle: Text(animal.description),
-          ),
-          ListTile(
-            leading: Icon(Icons.pets),
-            title: Text("Type"),
-            subtitle: Text(animal.type),
-          ),
-          ListTile(
-            leading: Icon(Icons.category),
-            title: Text("Breed"),
-            subtitle: Text(animal.breed),
-          ),
-          ListTile(
-            leading: Icon(Icons.color_lens),
-            title: Text("Color"),
-            subtitle: Text(animal.color),
-          ),
-          ListTile(
-            leading: Icon(Icons.cake),
-            title: Text("Age"),
-            subtitle: Text(animal.age.toString()),
-          ),
-          ListTile(
-            leading: Icon(Icons.directions_run),
-            title: Text("Activity Level"),
-            subtitle: Text(animal.activity_level),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Description',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  animal.description,
+                  style: const TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Divider(),
+                ListTile(
+                  leading: const Icon(Icons.pets),
+                  title: const Text("Type"),
+                  subtitle: Text(animal.type),
+                ),
+                ListTile(
+                  leading: const Icon(Icons.category),
+                  title: const Text("Breed"),
+                  subtitle: Text(animal.breed),
+                ),
+                ListTile(
+                  leading: const Icon(Icons.color_lens),
+                  title: const Text("Color"),
+                  subtitle: Text(animal.color),
+                ),
+                ListTile(
+                  leading: const Icon(Icons.cake),
+                  title: const Text("Age"),
+                  subtitle: Text("${animal.age} years"),
+                ),
+                ListTile(
+                  leading: const Icon(Icons.directions_run),
+                  title: const Text("Activity Level"),
+                  subtitle: Text(animal.activity_level),
+                ),
+              ],
+            ),
           ),
         ],
       ),
