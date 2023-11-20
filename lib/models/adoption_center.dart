b7e2e81b1f9c39d0de986715fb64243b6d8d66fe
DragosIsar <90556398+DragosIsar@users.noreply.github.com>
2023-11-20 19:42:42 +0100

    adds first ver of addAnimal and addAdoptionCenter

--- lib/models/adoption_center.dart
+++ lib/models/adoption_center.dart
@@ -1,32 +1,32 @@
+import 'package:uuid/uuid.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class AdoptionCenter {
-  final String id;
-  final String imageUrl;
-  final String name;
-  final String description;
-  final AdoptionCenterLocation location;
-  final List<String> availableAnimalIds;
+final uuid = Uuid();
 
+class AdoptionCenter {
   AdoptionCenter({
-    required this.id,
-    required this.imageUrl,
+    //required this.imageUrl,
     required this.name,
     required this.description,
     required this.location,
-    required this.availableAnimalIds,
-  });
+    this.animalIds,
+  }) : adoptionCenterId = uuid.v4();
+
+  //final String imageUrl;
+  final String adoptionCenterId;
+  final String name;
+  final String description;
+  final AdoptionCenterLocation location;
+  final List<String>? animalIds;
 }
 
 class AdoptionCenterLocation {
-  final LatLng location;
   final String street;
   final String city;
   final String zipCode;
   final String country;
 
   AdoptionCenterLocation({
-    required this.location,
     required this.street,
     required this.city,
     required this.zipCode,
