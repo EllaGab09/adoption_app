d65858bf3990008c609aa74e6674dff3034ed019
AndreasH <Andreashavnen@gmail.com>
2023-11-29 01:51:18 +0100

    Removed the overflow in add animals

--- lib/screens/add_animal_screen.dart
+++ lib/screens/add_animal_screen.dart
@@ -133,197 +133,200 @@ class _AddAnimalFormState extends State<AddAnimalForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.adoptionCenter.name),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Center(
-                child: Text('Add Animal',
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.displaySmall!.copyWith(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 24,
-                        )),
-              ),
-
-              // NAME
-              TextFormField(
-                controller: _nameController,
-                decoration: InputDecoration(
-                  labelText: 'Name',
-                  // Apply theme style for input field
-                  labelStyle: Theme.of(context).textTheme.titleMedium,
-                  // Apply theme style for error text
-                  errorStyle: Theme.of(context).textTheme.bodySmall,
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a name';
-                  }
-                  return null;
-                },
-              ),
-
-              // IMAGE URL
-              TextFormField(
-                controller: _imageURLController,
-                decoration: InputDecoration(
-                  labelText: 'Image URL',
-                  // Apply theme style for input field
-                  labelStyle: Theme.of(context).textTheme.titleMedium,
-                  // Apply theme style for error text
-                  errorStyle: Theme.of(context).textTheme.bodySmall,
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter an image URL';
-                  }
-                  return null;
-                },
-              ),
-
-              // DESCRIPTION
-              TextFormField(
-                controller: _descriptionController,
-                decoration: InputDecoration(
-                  labelText: 'Description',
-                  // Apply theme style for input field
-                  labelStyle: Theme.of(context).textTheme.titleMedium,
-                  // Apply theme style for error text
-                  errorStyle: Theme.of(context).textTheme.bodySmall,
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a description';
-                  }
-                  return null;
-                },
-              ),
-
-              // ANIMAL TYPE
-              DropdownButton<AnimalType>(
-                value: animalType,
-                onChanged: (AnimalType? newValue) {
-                  if (newValue != null) {
-                    setState(() {
-                      _setAnimalType(newValue);
-                      _breedController.text = (newValue ==
-                              AnimalType.unspecified)
-                          ? "" // Clear the breed controller if the type is unspecified
-                          : breedOptions[newValue]?.first ?? "";
-                    });
-                  }
-                },
-                items: widget.animal!
-                    .getAnimalTypes()
-                    .map<DropdownMenuItem<AnimalType>>((AnimalType value) {
-                  return DropdownMenuItem<AnimalType>(
-                    value: value,
-                    child: Text(value.toString().split('.').last),
-                  );
-                }).toList(),
-              ),
-
-              // BREED
-
-              DropdownButton<String>(
-                value: _breedController.text,
-                onChanged: (String? newValue) {
-                  setState(() {
-                    _breedController.text = newValue!;
-                  });
-                },
-                items: (animalType == AnimalType.unspecified)
-                    ? [
-                        const DropdownMenuItem(
-                          value: "",
-                          child: Text(
-                              "Select Type First"), // Placeholder for unspecified type
-                        ),
-                      ]
-                    : breedOptions[animalType]
-                            ?.map<DropdownMenuItem<String>>((String breed) {
-                          return DropdownMenuItem<String>(
-                            value: breed,
-                            child: Text(breed),
-                          );
-                        }).toList() ??
-                        [],
-              ),
-
-              DropdownButton<AnimalActivity>(
-                value: activityLevel,
-                onChanged: (AnimalActivity? newValue) {
-                  if (newValue != null) {
-                    _setAnimalActivityLevel(newValue);
-                  }
-                },
-                items: widget.animal!
-                    .getActivityLevels()
-                    .map<DropdownMenuItem<AnimalActivity>>(
-                        (AnimalActivity value) {
-                  return DropdownMenuItem<AnimalActivity>(
-                    value: value,
-                    child: Text(value.toString().split('.').last),
-                  );
-                }).toList(),
-              ),
-              // ANIMAL SEX
-              DropdownButton<AnimalSex>(
-                value: animalSex,
-                onChanged: (AnimalSex? newValue) {
-                  if (newValue != null) {
-                    _setAnimalSex(newValue);
-                  }
-                },
-                items: widget.animal!
-                    .getSex()
-                    .map<DropdownMenuItem<AnimalSex>>((AnimalSex value) {
-                  return DropdownMenuItem<AnimalSex>(
-                    value: value,
-                    child: Text(value.toString().split('.').last),
-                  );
-                }).toList(),
-              ),
-
-              // AGE
-              TextFormField(
-                controller: _ageController,
-                decoration: InputDecoration(
-                  labelText: 'Age', // Apply theme style for input field
-                  labelStyle: Theme.of(context).textTheme.titleMedium,
-                  errorStyle: Theme.of(context).textTheme.bodySmall,
-                ),
-                keyboardType:
-                    TextInputType.number, // Set the keyboard type to number
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter an age';
-                  }
-                  if (int.tryParse(value) == null) {
-                    return 'Age must be a number';
-                  }
-                  return null;
-                },
-              ),
-
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  onPressed(context);
-                },
-                child: const Text('Save'),
-              ),
-            ],
-          ),
+        appBar: AppBar(
+          title: Text(widget.adoptionCenter.name),
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          reverse: true,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Center(
+                    child: Text('Add Animal',
+                        textAlign: TextAlign.center,
+                        style:
+                            Theme.of(context).textTheme.displaySmall!.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24,
+                                )),
+                  ),
+
+                  // NAME
+                  TextFormField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      labelText: 'Name',
+                      // Apply theme style for input field
+                      labelStyle: Theme.of(context).textTheme.titleMedium,
+                      // Apply theme style for error text
+                      errorStyle: Theme.of(context).textTheme.bodySmall,
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a name';
+                      }
+                      return null;
+                    },
+                  ),
+
+                  // IMAGE URL
+                  TextFormField(
+                    controller: _imageURLController,
+                    decoration: InputDecoration(
+                      labelText: 'Image URL',
+                      // Apply theme style for input field
+                      labelStyle: Theme.of(context).textTheme.titleMedium,
+                      // Apply theme style for error text
+                      errorStyle: Theme.of(context).textTheme.bodySmall,
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter an image URL';
+                      }
+                      return null;
+                    },
+                  ),
+
+                  // DESCRIPTION
+                  TextFormField(
+                    controller: _descriptionController,
+                    decoration: InputDecoration(
+                      labelText: 'Description',
+                      // Apply theme style for input field
+                      labelStyle: Theme.of(context).textTheme.titleMedium,
+                      // Apply theme style for error text
+                      errorStyle: Theme.of(context).textTheme.bodySmall,
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a description';
+                      }
+                      return null;
+                    },
+                  ),
+
+                  // ANIMAL TYPE
+                  DropdownButton<AnimalType>(
+                    value: animalType,
+                    onChanged: (AnimalType? newValue) {
+                      if (newValue != null) {
+                        setState(() {
+                          _setAnimalType(newValue);
+                          _breedController.text = (newValue ==
+                                  AnimalType.unspecified)
+                              ? "" // Clear the breed controller if the type is unspecified
+                              : breedOptions[newValue]?.first ?? "";
+                        });
+                      }
+                    },
+                    items: widget.animal!
+                        .getAnimalTypes()
+                        .map<DropdownMenuItem<AnimalType>>((AnimalType value) {
+                      return DropdownMenuItem<AnimalType>(
+                        value: value,
+                        child: Text(value.toString().split('.').last),
+                      );
+                    }).toList(),
+                  ),
+
+                  // BREED
+
+                  DropdownButton<String>(
+                    value: _breedController.text,
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _breedController.text = newValue!;
+                      });
+                    },
+                    items: (animalType == AnimalType.unspecified)
+                        ? [
+                            const DropdownMenuItem(
+                              value: "",
+                              child: Text(
+                                  "Select Type First"), // Placeholder for unspecified type
+                            ),
+                          ]
+                        : breedOptions[animalType]
+                                ?.map<DropdownMenuItem<String>>((String breed) {
+                              return DropdownMenuItem<String>(
+                                value: breed,
+                                child: Text(breed),
+                              );
+                            }).toList() ??
+                            [],
+                  ),
+
+                  DropdownButton<AnimalActivity>(
+                    value: activityLevel,
+                    onChanged: (AnimalActivity? newValue) {
+                      if (newValue != null) {
+                        _setAnimalActivityLevel(newValue);
+                      }
+                    },
+                    items: widget.animal!
+                        .getActivityLevels()
+                        .map<DropdownMenuItem<AnimalActivity>>(
+                            (AnimalActivity value) {
+                      return DropdownMenuItem<AnimalActivity>(
+                        value: value,
+                        child: Text(value.toString().split('.').last),
+                      );
+                    }).toList(),
+                  ),
+                  // ANIMAL SEX
+                  DropdownButton<AnimalSex>(
+                    value: animalSex,
+                    onChanged: (AnimalSex? newValue) {
+                      if (newValue != null) {
+                        _setAnimalSex(newValue);
+                      }
+                    },
+                    items: widget.animal!
+                        .getSex()
+                        .map<DropdownMenuItem<AnimalSex>>((AnimalSex value) {
+                      return DropdownMenuItem<AnimalSex>(
+                        value: value,
+                        child: Text(value.toString().split('.').last),
+                      );
+                    }).toList(),
+                  ),
+
+                  // AGE
+                  TextFormField(
+                    controller: _ageController,
+                    decoration: InputDecoration(
+                      labelText: 'Age', // Apply theme style for input field
+                      labelStyle: Theme.of(context).textTheme.titleMedium,
+                      errorStyle: Theme.of(context).textTheme.bodySmall,
+                    ),
+                    keyboardType:
+                        TextInputType.number, // Set the keyboard type to number
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter an age';
+                      }
+                      if (int.tryParse(value) == null) {
+                        return 'Age must be a number';
+                      }
+                      return null;
+                    },
+                  ),
+
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      onPressed(context);
+                    },
+                    child: const Text('Save'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
