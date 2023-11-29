a86a8dcc501ab6ff0cdab24f724d441195123732
Gabriela Istrate <gistrate09@gmail.com>
2023-11-29 19:57:36 +0100

    feat: create adoption application model

--- lib/models/adoption_application.dart
+++ lib/models/adoption_application.dart
@@ -5,8 +5,16 @@ const uuid = Uuid();
 class Adoption_Application {
   Adoption_Application({
     required this.userId,
+    required this.animalId,
+    required this.applicationStatus,
+    required this.applicationDate,
+    required this.applicationMessage,
   }) : applicationId = uuid.v4();
 
   final String applicationId;
   final String userId;
+  final String animalId;
+  final String applicationStatus;
+  final String applicationDate; //?
+  final String applicationMessage;
 }
