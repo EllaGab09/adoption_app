7b3a3a10c19e15175cf169e928dedc4b3eb8990b
AndreasH <Andreashavnen@gmail.com>
2023-11-30 01:21:13 +0100

    removed unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:adoption_app/widgets/tabs.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'themes/theme_data.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
--- lib/themes/theme_data.dart
+++ lib/themes/theme_data.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter/material.dart';
-
 final ThemeData lightTheme = ThemeData(
   applyElevationOverlayColor: false,
   brightness: Brightness.light,
