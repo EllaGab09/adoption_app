0af2fab6ca9ffb1d32f68958b7a78b066470e270
AndreasH <Andreashavnen@gmail.com>
2023-11-30 23:50:31 +0100

    Update inbox_screen.dart

--- lib/screens/inbox_screen.dart
+++ lib/screens/inbox_screen.dart
@@ -6,10 +6,13 @@ import 'package:adoption_app/providers/applications_provider.dart';
 
 /// A screen that displays the inbox for applications.
 ///
-/// This screen is responsible for showing the user's applications, where they can view and manage their applications.
-/// It extends the [ConsumerWidget] class, allowing it to rebuild when the state of the underlying data changes.
+/// This screen is responsible for showing the user's applications,
+/// where they can view and manage their applications.
+/// It extends the [ConsumerWidget] class, allowing it to rebuild
+/// when the state of the underlying data changes.
 class InboxScreen extends ConsumerWidget {
-  const InboxScreen({super.key});
+  // InboxScreen constructor
+  const InboxScreen({Key? key});
 
   @override
 
@@ -18,11 +21,15 @@ class InboxScreen extends ConsumerWidget {
   /// This method is responsible for constructing the UI of the inbox screen.
   /// It takes in the [context] and [ref] parameters and returns a widget.
   Widget build(BuildContext context, WidgetRef ref) {
+    // Get the list of applications from the provider
     final applications = ref.watch(applicationProvider);
+
     return Scaffold(
+      // App bar with the custom logo
       appBar: const LogoAppBar(),
       body: applications.isEmpty
-          ? const Center(
+          ? // Display a message if there are no applications
+          const Center(
               child: Text(
                 'No applications yet.',
                 style: TextStyle(
@@ -34,9 +41,11 @@ class InboxScreen extends ConsumerWidget {
                 ),
               ),
             )
-          : ListView.builder(
+          : // Display a list of application items if there are applications
+          ListView.builder(
               itemCount: applications.length,
               itemBuilder: (ctx, i) => ApplicationItem(
+                // Pass details of each application to the ApplicationItem widget
                 animalName: applications[i].animalName,
                 id: applications[i].id,
                 userName: applications[i].userName,
